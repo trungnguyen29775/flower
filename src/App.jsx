adec01a270c1f3470c9e6d27a2b4c07d52f84e43
trungnguyen29775 <91877086+npdqba02@users.noreply.github.com>
2024-07-05 23:27:24 +0700

    5/7/2024: Done

---
+++ src/App.jsx
@@ -0,0 +1,211 @@
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("container");
+      clearTimeout(c);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    onload();
+  }, []);
+
+  return (
+    <>
+      <div className="night"></div>
+      <div className="flowers">
+        <div className="flower flower--1">
+          <div className="flower__leafs flower__leafs--1">
+            <div className="flower__leaf flower__leaf--1"></div>
+            <div className="flower__leaf flower__leaf--2"></div>
+            <div className="flower__leaf flower__leaf--3"></div>
+            <div className="flower__leaf flower__leaf--4"></div>
+            <div className="flower__white-circle"></div>
+
+            <div className="flower__light flower__light--1"></div>
+            <div className="flower__light flower__light--2"></div>
+            <div className="flower__light flower__light--3"></div>
+            <div className="flower__light flower__light--4"></div>
+            <div className="flower__light flower__light--5"></div>
+            <div className="flower__light flower__light--6"></div>
+            <div className="flower__light flower__light--7"></div>
+            <div className="flower__light flower__light--8"></div>
+          </div>
+          <div className="flower__line">
+            <div className="flower__line__leaf flower__line__leaf--1"></div>
+            <div className="flower__line__leaf flower__line__leaf--2"></div>
+            <div className="flower__line__leaf flower__line__leaf--3"></div>
+            <div className="flower__line__leaf flower__line__leaf--4"></div>
+            <div className="flower__line__leaf flower__line__leaf--5"></div>
+            <div className="flower__line__leaf flower__line__leaf--6"></div>
+          </div>
+        </div>
+
+        <div className="flower flower--2">
+          <div className="flower__leafs flower__leafs--2">
+            <div className="flower__leaf flower__leaf--1"></div>
+            <div className="flower__leaf flower__leaf--2"></div>
+            <div className="flower__leaf flower__leaf--3"></div>
+            <div className="flower__leaf flower__leaf--4"></div>
+            <div className="flower__white-circle"></div>
+
+            <div className="flower__light flower__light--1"></div>
+            <div className="flower__light flower__light--2"></div>
+            <div className="flower__light flower__light--3"></div>
+            <div className="flower__light flower__light--4"></div>
+            <div className="flower__light flower__light--5"></div>
+            <div className="flower__light flower__light--6"></div>
+            <div className="flower__light flower__light--7"></div>
+            <div className="flower__light flower__light--8"></div>
+          </div>
+          <div className="flower__line">
+            <div className="flower__line__leaf flower__line__leaf--1"></div>
+            <div className="flower__line__leaf flower__line__leaf--2"></div>
+            <div className="flower__line__leaf flower__line__leaf--3"></div>
+            <div className="flower__line__leaf flower__line__leaf--4"></div>
+          </div>
+        </div>
+
+        <div className="flower flower--3">
+          <div className="flower__leafs flower__leafs--3">
+            <div className="flower__leaf flower__leaf--1"></div>
+            <div className="flower__leaf flower__leaf--2"></div>
+            <div className="flower__leaf flower__leaf--3"></div>
+            <div className="flower__leaf flower__leaf--4"></div>
+            <div className="flower__white-circle"></div>
+
+            <div className="flower__light flower__light--1"></div>
+            <div className="flower__light flower__light--2"></div>
+            <div className="flower__light flower__light--3"></div>
+            <div className="flower__light flower__light--4"></div>
+            <div className="flower__light flower__light--5"></div>
+            <div className="flower__light flower__light--6"></div>
+            <div className="flower__light flower__light--7"></div>
+            <div className="flower__light flower__light--8"></div>
+          </div>
+          <div className="flower__line">
+            <div className="flower__line__leaf flower__line__leaf--1"></div>
+            <div className="flower__line__leaf flower__line__leaf--2"></div>
+            <div className="flower__line__leaf flower__line__leaf--3"></div>
+            <div className="flower__line__leaf flower__line__leaf--4"></div>
+          </div>
+        </div>
+
+        <div className="grow-ans" style={{ '--d': '1.2s' }}>
+          <div className="flower__g-long">
+            <div className="flower__g-long__top"></div>
+            <div className="flower__g-long__bottom"></div>
+          </div>
+        </div>
+
+        <div className="growing-grass">
+          <div className="flower__grass flower__grass--1">
+            <div className="flower__grass--top"></div>
+            <div className="flower__grass--bottom"></div>
+            <div className="flower__grass__leaf flower__grass__leaf--1"></div>
+            <div className="flower__grass__leaf flower__grass__leaf--2"></div>
+            <div className="flower__grass__leaf flower__grass__leaf--3"></div>
+            <div className="flower__grass__leaf flower__grass__leaf--4"></div>
+            <div className="flower__grass__leaf flower__grass__leaf--5"></div>
+            <div className="flower__grass__leaf flower__grass__leaf--6"></div>
+            <div className="flower__grass__leaf flower__grass__leaf--7"></div>
+            <div className="flower__grass__leaf flower__grass__leaf--8"></div>
+            <div className="flower__grass__overlay"></div>
+          </div>
+        </div>
+
+        <div className="growing-grass">
+          <div className="flower__grass flower__grass--2">
+            <div className="flower__grass--top"></div>
+            <div className="flower__grass--bottom"></div>
+            <div className="flower__grass__leaf flower__grass__leaf--1"></div>
+            <div className="flower__grass__leaf flower__grass__leaf--2"></div>
+            <div className="flower__grass__leaf flower__grass__leaf--3"></div>
+            <div className="flower__grass__leaf flower__grass__leaf--4"></div>
+            <div className="flower__grass__leaf flower__grass__leaf--5"></div>
+            <div className="flower__grass__leaf flower__grass__leaf--6"></div>
+            <div className="flower__grass__leaf flower__grass__leaf--7"></div>
+            <div className="flower__grass__leaf flower__grass__leaf--8"></div>
+            <div className="flower__grass__overlay"></div>
+          </div>
+        </div>
+
+        <div className="grow-ans" style={{ '--d': '2.4s' }}>
+          <div className="flower__g-right flower__g-right--1">
+            <div className="leaf"></div>
+          </div>
+        </div>
+
+        <div className="grow-ans" style={{ '--d': '2.8s' }}>
+          <div className="flower__g-right flower__g-right--2">
+            <div className="leaf"></div>
+          </div>
+        </div>
+
+        <div className="grow-ans" style={{ '--d': '2.8s' }}>
+          <div className="flower__g-front">
+            <div className="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--1">
+              <div className="flower__g-front__leaf"></div>
+            </div>
+            <div className="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--2">
+              <div className="flower__g-front__leaf"></div>
+            </div>
+            <div className="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--3">
+              <div className="flower__g-front__leaf"></div>
+            </div>
+            <div className="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--4">
+              <div className="flower__g-front__leaf"></div>
+            </div>
+            <div className="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--5">
+              <div className="flower__g-front__leaf"></div>
+            </div>
+            <div className="flower__g-front__leaf-wrapper flower__g-front__leaf-wrapper--6">
+              <div className="flower__g-front__leaf"></div>
+            </div>
+          </div>
+        </div>
+
+        <div className="grow-ans" style={{ '--d': '3.6s' }}>
+          <div className="flower__g-left flower__g-left--1">
+            <div className="leaf"></div>
+          </div>
+        </div>
+
+        <div className="grow-ans" style={{ '--d': '4.2s' }}>
+          <div className="flower__g-left flower__g-left--2">
+            <div className="leaf"></div>
+          </div>
+        </div>
+
+        <div className="grow-ans" style={{ '--d': '4.2s' }}>
+          <div className="flower__g-bottom">
+            <div className="flower__g-bottom__leaf-wrapper flower__g-bottom__leaf-wrapper--1">
+              <div className="flower__g-bottom__leaf"></div>
+            </div>
+            <div className="flower__g-bottom__leaf-wrapper flower__g-bottom__leaf-wrapper--2">
+              <div className="flower__g-bottom__leaf"></div>
+            </div>
+            <div className="flower__g-bottom__leaf-wrapper flower__g-bottom__leaf-wrapper--3">
+              <div className="flower__g-bottom__leaf"></div>
+            </div>
+            <div className="flower__g-bottom__leaf-wrapper flower__g-bottom__leaf-wrapper--4">
+              <div className="flower__g-bottom__leaf"></div>
+            </div>
+            <div className="flower__g-bottom__leaf-wrapper flower__g-bottom__leaf-wrapper--5">
+              <div className="flower__g-bottom__leaf"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
